9a647e07060bd0a7f2a1d5b750431c2bec5dd48c
Noriaki UCHIYAMA <uchiyama.noriaki@gmail.com>
2014-01-01 15:47:55 +0900

    - added auto-ruby-mode to Procfile

--- .emacs.d/inits/30-ruby.el
+++ .emacs.d/inits/30-ruby.el
@@ -4,6 +4,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Procfile$" . ruby-mode))
 
 (when (require 'ruby-mode nil t)
   (defun my/ruby-mode-hook-function ()
